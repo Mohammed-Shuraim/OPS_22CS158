900063a3daf05e5a84717575fff0fac685bc4536
Mohammed-Shuraim <160753433+Mohammed-Shuraim@users.noreply.github.com>
2024-06-05 21:30:48 +0530

    multi.c

--- multi.c
+++ multi.c
@@ -1,135 +1,96 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-#define MAX 20
-#define TQ 4
+#include <limits.h>
 
 struct Proc {
-    int id, at, bt, pri, rt, ct, tat, wt, exec, rem, q;
+    int id;  
+    int bt;   
+    int at;   
+    int qt;   
+    int wt;  
+    int tat;  
+    int rt;   
+    int st;  
+    int et;   
+    int vis;  
 };
 
-int next_fcfs(struct Proc p[], int n, int t) {
-    int e = -1;
-    for (int i = 0; i < n; i++) {
-        if (p[i].at <= t && p[i].exec == 0 && p[i].q == 1) {
-            if (e == -1 || p[i].at < p[e].at) {
-                e = i;
-            }
-        }
-    }
-    return e;
-}
+void main () 
+{
+    int n, currTime = 0;
+    float avgWT = 0, avgTAT = 0, avgRT = 0, tp;
 
-int next_sjf(struct Proc p[], int n, int t) {
-    int s = -1;
-    for (int i = 0; i < n; i++) {
-        if (p[i].at <= t && p[i].exec == 0 && p[i].q == 2) {
-            if (s == -1 || p[i].bt < p[s].bt) {
-                s = i;
-            }
-        }
-    }
-    return s;
-}
-
-int next_pri(struct Proc p[], int n, int t) {
-    int h = -1;
-    for (int i = 0; i < n; i++) {
-        if (p[i].at <= t && p[i].exec == 0 && p[i].q == 3) {
-            if (h == -1 || p[i].pri < p[h].pri) {
-                h = i;
-            }
-        }
-    }
-    return h;
-}
-
-void rr(struct Proc p[], int n, int tq, int *t) {
-    int d = 0;
-    while (d < n) {
-        for (int i = 0; i < n; i++) {
-            if (p[i].at <= *t && p[i].rem > 0 && p[i].q == 4) {
-                if (p[i].rem > tq) {
-                    *t += tq;
-                    p[i].rem -= tq;
-                } else {
-                    *t += p[i].rem;
-                    p[i].rem = 0;
-                    p[i].ct = *t;
-                    p[i].tat = p[i].ct - p[i].at;
-                    p[i].wt = p[i].tat - p[i].bt;
-                    if (p[i].wt < 0)
-                        p[i].wt = 0;
-                    d++;
-                }
-            }
-        }
-    }
-}
-
-void simulate(struct Proc p[], int n) {
-    int t = 0, comp = 0;
-
-    while (comp < n) {
-        int np = -1;
-
-        np = next_fcfs(p, n, t);
-        if (np == -1) np = next_sjf(p, n, t);
-        if (np == -1) np = next_pri(p, n, t);
-        
-        if (np != -1) {
-            t += p[np].bt;
-            p[np].ct = t;
-            p[np].tat = p[np].ct - p[np].at;
-            p[np].wt = p[np].tat - p[np].bt;
-            if (p[np].wt < 0)
-                p[np].wt = 0;
-            p[np].exec = 1;
-            comp++;
-        } else {
-            t++;
-        }
-
-        if (comp < n && np == -1) {
-            rr(p, n, TQ, &t);
-            for (int i = 0; i < n; i++) {
-                if (p[i].exec == 1 && p[i].q == 4) {
-                    comp++;
-                }
-            }
-        }
-    }
-}
-
-int main() {
-    int n;
-    struct Proc p[MAX];
-
-    printf("Enter the number of processes: ");
+    printf("Queue 1 is system process\nQueue 2 is User Process\n");
+    printf("Enter number of processes: ");
     scanf("%d", &n);
 
+    struct Proc procs[n], tmp;
+
     for (int i = 0; i < n; i++) {
-        p[i].id = i + 1;
-        printf("Enter arrival time for process %d: ", i + 1);
-        scanf("%d", &p[i].at);
-        printf("Enter burst time for process %d: ", i + 1);
-        scanf("%d", &p[i].bt);
-        printf("Enter priority for process %d: ", i + 1);
-        scanf("%d", &p[i].pri);
-        printf("Enter queue number for process %d (1: FCFS, 2: SJF, 3: Priority, 4: RR): ", i + 1);
-        scanf("%d", &p[i].q);
-        p[i].rem = p[i].bt;
-        p[i].exec = 0;
+        procs[i].id = i + 1;
+        procs[i].vis = 0;
+        printf("Enter Burst Time, Arrival Time and Queue of P%d: ", i + 1);
+        scanf("%d %d %d", &procs[i].bt, &procs[i].at, &procs[i].qt);
     }
 
-    simulate(p, n);
-
-    printf("ID\tAT\tBT\tPri\tQ\tCT\tWT\tTAT\tRT\n");
     for (int i = 0; i < n; i++) {
-        printf("%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\n",
-               p[i].id, p[i].at, p[i].bt, p[i].pri,
-               p[i].q, p[i].ct, p[i].wt, p[i].tat, p[i].rt);
+        for (int j = i + 1; j < n; j++) {
+            if (procs[i].at > procs[j].at ||
+                (procs[i].at == procs[j].at && procs[i].qt > procs[j].qt)) {
+                tmp = procs[i];
+                procs[i] = procs[j];
+                procs[j] = tmp;
+            }
+        }
     }
 
-    return 0;
+    int compTime = procs[0].bt, minQT = INT_MAX, minQI, procCount = 0;
+    procs[0].vis = 1;
+
+    while (procCount < n - 1) {
+        for (int i = 0; i < n; i++) {
+            if (procs[i].at <= compTime && procs[i].vis == 0) {
+                if (procs[i].qt < minQT) {
+                    minQT = procs[i].qt;
+                    minQI = i;
+                }
+            }
+        }
+
+        procs[minQI].vis = 1;
+        compTime += procs[minQI].bt;
+        minQT = INT_MAX;
+
+        tmp = procs[procCount + 1];
+        procs[procCount + 1] = procs[minQI];
+        procs[minQI] = tmp;
+
+        procCount++;
+    }
+
+    printf("\nProcess\tWT\tTAT\tRT\n");
+    for (int i = 0; i < n; i++) {
+        if (procs[i].at < currTime) {
+            procs[i].st = currTime;
+        } else {
+            procs[i].st = procs[i].at;
+        }
+        procs[i].et = procs[i].st + procs[i].bt;
+        currTime += procs[i].bt;
+
+        procs[i].tat = procs[i].et - procs[i].at;
+        procs[i].wt = procs[i].tat - procs[i].bt;
+        procs[i].rt = procs[i].st - procs[i].at;
+
+        printf("%d\t%d\t%d\t%d\n", procs[i].id, procs[i].wt, procs[i].tat, procs[i].rt);
+
+        avgWT += procs[i].wt;
+        avgTAT += procs[i].tat;
+        avgRT += procs[i].rt;
+    }
+
+    tp = (float)procs[n - 1].et / n;
+    printf("Average WT: %.2f\n", avgWT / n);
+    printf("Average TAT: %.2f\n", avgTAT / n);
+    printf("Average RT: %.2f\n", avgRT / n);
+    printf("Throughput: %.2f", tp);
 }
